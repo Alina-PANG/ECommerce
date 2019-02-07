601a5ae883f9198beb266c9f4f3d54519bee1a3a
Pang Hangzhi <hangzhipang@gmail.com>
2019-02-07 21:03:20 +0800

    finish the 3 html pages in IS3106 lab 3

---
+++ js/products-data-generator.js
@@ -0,0 +1,56 @@
+
+
+function tableGenerateViewAllProducts() {
+  console.log('being run');
+  var table = document.getElementById('product-table');
+  for (var i = 0; i < 9; i ++) {
+    var tr = document.createElement('tr');
+    for(var j = 0; j < 9; j ++) {
+      var td = document.createElement('td');
+      switch(j) {
+        case 0:
+          td.innerHTML = i + 1;
+        break;
+        case 1:
+          td.innerHTML = "PROD00" + (i + 1);
+        break;
+        case 2:
+        case 3:
+        case 8:
+          var ch;
+          switch((i)/3){
+            case 0:
+              ch = "A";
+              break;
+            case 1:
+              ch = "B";
+              break;
+            case 2:
+              ch = "C";
+              break;
+          }
+          if (j == 7){
+            td.innerHTML = "Category " + ch;
+            break;
+          }
+          td.innerHTML = "Product " + ch + i%3;
+          break;
+        case 4:
+          td.innerHTML = 8;
+          break;
+        case 5:
+          td.innerHTML = 10;
+          break;
+        case 6: // Reorder
+          td.innerHTML = 5
+          break;
+        case 7: // Unit Price
+          td.innerHTML = "$10.00"
+        break;
+      }
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+}
+tableGenerateViewAllProducts();
